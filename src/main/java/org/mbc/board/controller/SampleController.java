cae4b3ef6ce8062818f85750523c443e7758c6fc
220-29\jo <wkddhr36@gmail.com>
2025-07-16 15:40:23 +0900

    보드 CRUD 완료

--- src/main/java/org/mbc/board/controller/SampleController.java
+++ src/main/java/org/mbc/board/controller/SampleController.java
@@ -1,37 +1,51 @@
 package org.mbc.board.controller;
 
+import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-import java.util.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
 public class SampleController {
+    // 컨트롤러는 url 생성과 프론트를 연결하는 부분으로 과거에 servlet-context.xml과 같은 역할을 함
 
-    @GetMapping("/hello")
-    public void hello(Model model) {
-        log.info("hello.......");
+    @GetMapping("/hello")  //  http://192.168.111.105:80/hello -> void -> hello.html
+    public void hello(Model model){
 
-        model.addAttribute("msg", "HELLO WORLD");
+        log.info("SampleController.hello메서드 실행.....");
+
+        model.addAttribute("msg", "안녕하세요 자바에 종점입니다.!!!!");
+
     }
 
-    @GetMapping("/ex/ex1")
+    @GetMapping("/ex/ex1") // http://192.168.111.105/ex/ex1 -> /resources/templates/ex/ex1.html
     public void ex1(Model model){
-        List<String> list = Arrays.asList("최장옥", "조은아", "김채하", "윤제석", "이은진");
-        model.addAttribute("list", list);
+        // 리스트타입으로 데이터를 보내 보자.
+        List<String> list = Arrays.asList("김기원", "이현우","홍경훈","박채은","양지민");
+
+        model.addAttribute("list",list);
+
     }
 
-    // 이너 클래스 생성, 안쪽에 클래스를 선언할 때 활용된다.
-    class SampleDTO {
-        //필드
+    @ToString
+    class SampleDTO{
+        // 이너 클래스로 클래스 안쪽에 클래스를 선언할 때 활용 된다.
+        // 필드
         private String p1, p2, p3;
 
-        // 생성자
+        // 기본생성자
 
+
+        // 메서드
         public String getP1() {
             return p1;
         }
@@ -43,32 +57,40 @@ public class SampleController {
         public String getP3() {
             return p3;
         }
-        // 메서드
-    }
+    } // 이너 클래스 종료
 
     @GetMapping("/ex/ex2")
-    public void ex2(Model model) {
-        log.info("ex/ex2...........");
+    public void ex2(Model model){
+        log.info("SampleController.ex2 메서드 실행....");
+        // 이너 클래스를 사용해서 객체를 뿌려보자.
 
-        // int형으로 정수를 뿌림 1~10까지 정수를 생성한다.
-        List<String> stringList = IntStream.range(1,10)
-                .mapToObj(i->"Data" +i)
-                .collect(Collectors.toList()); //리스트에 정수(숫자) 문자열이 생성됨
-       // [데이터 1, 데아터 2,...... 데이터 9]
-        model.addAttribute("list", stringList); // 만든 객체 값
+        List<String> strList = IntStream.range(1,10) // 1~10까지 정수를 생성한다.
+                .mapToObj(i -> "데이터"+i)
+                .collect(Collectors.toList());  // 리스트에 정수(숫자)문자열이 생성된다.
+        // [데이터1, 데이터2 ...... 데이터9]
 
-        Map<String, String> map = new HashMap<>(); // 모두 string형이어서 다 string으로 한다.
-        map.put("id", "choi");
-        map.put("pw", "1234"); // key, value
+        Map<String,String> map = new HashMap<>();
+        map.put("id","kkw");
+        map.put("pw","1234"); // key , value
 
-        model.addAttribute("map", map); // 만든 객체 값
+        SampleDTO sampleDTO =new SampleDTO();
+        sampleDTO.p1 = "값....  p1";
+        sampleDTO.p2 = "값....  p2";
+        sampleDTO.p3 = "값....  p3";
+        // 최종적으로 객체 3개 완성 됨. 
+        
+        model.addAttribute("list",strList); // 65행에 만들어진 객체 값
+        model.addAttribute("map",map);      // 70행에 만들어진 객체 값
+        model.addAttribute("dto",sampleDTO); // 74행에 만들어진 객체 값
 
-        SampleDTO sampleDTO = new SampleDTO();
-        sampleDTO.p1="value -- p1";
-        sampleDTO.p2="value -- p2";
-        sampleDTO.p3="value -- p3";
-        // 최종적으로 객체 3개 완성 됨
+        // 리턴타입이 void 임으로  /resources/templates/ex/ex2.html
+    }
 
-        model.addAttribute("dto", sampleDTO); // 만든 객체 값
+    @GetMapping("/ex/ex3") //http://192.168.111.105/ex/ex3 -> /resources/templates/ex/ex3.html
+    public void ex3(Model model){
+        log.info("SampleController.ex3 메서드 실행....");
+
+        model.addAttribute("arr", new String[]{"전민기, 김진우, 전혜진"});
+
     }
 }
---
+++ src/test/java/org/mbc/board/repository/BoardRepositoryTests.java
@@ -0,0 +1,121 @@
+package org.mbc.board.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.mbc.board.domain.Board;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest // 메서드용 테스트 동작
+@Log4j2 // 로그용
+public class BoardRepositoryTests {
+    // 영속성 계층에 테스트용
+
+    @Autowired // 생성자 자동 주입
+    private BoardRepository boardRepository;
+
+    @Test
+    public void testInsert(){
+        // 데이터베이스에 데이터 주입(c) 테스트 코드
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            // i 변수에 1~100까지 100개의 정수를 반복해서 생성
+            Board board = Board.builder()
+                    .title("제목..."+i)  // board.setTitle()
+                    .content("내용..."+i) // board.setContent()
+                    .writer("user"+(i%10))  // board.setWriter()
+                    .build(); // @Builder 용 (세터 대신 좀더 간단하고 가독성 좋게 )
+                // log.info((board));
+            Board result = boardRepository.save(board) ; // 데이터베이스에 기록하는 코드
+            //                            .save 메서드는 jpa에서 상속한 메서드로 값을 저장하는 용도
+            //                                          이미 값이 있으면 update를 진행한다.
+            log.info("게시물 번호 출력 : " + result.getBno() + "게시물의 제목 : " + result.getTitle());
+
+                }// forEach문 종료
+        );// IntStream. 종료
+
+    }
+
+    @Test
+    public void testSelect() {
+        Long bno = 100L; //게시물 번호가 100인 개체를 확인해보자
+
+        Optional<Board> result = boardRepository.findById(bno);
+        // optional 널값이 나올 경우를 대비한 객체
+        // findById=select * from board where bno=bno
+
+        Board board=result.orElseThrow(); //값이 있으면 넣어라
+
+        log.info(bno + "가 데이터 베이스에 존재합니다.");
+        log.info(board);
+    }
+
+    @Test
+    public void upDate() {
+        Long bno = 100L; //100번째 게시물을 가져와서 수정 후 테스트 종료
+        Optional<Board> result = boardRepository.findById(bno); //bno를 찾아서 result에 넣는다
+        Board board = result.orElseThrow(); // 가져온 값이 있으면 board 타입에 객체에 넣는다
+        board.change("수정 테스트 제목", "수정 테스트 내용");// 제목과 내용만 수정할 수 잇는 메서드
+        boardRepository.save(board);
+    }
+
+    @Test
+    public void testDelete() {
+        Long bno = 1L;
+        boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testPaging() {
+        //.fildAll() 는 모든 리스트를 출력하는 메서드 select * from board
+        // 전체 리스트에 페이징과 정렬 기법도 추가해보자.
+
+        Pageable pageable = PageRequest.of(19, 5, Sort.by("bno").descending());
+        // 시작 번호, 10개 목록, 번호를 기준으로 내림차순 정렬
+
+//        Hibernate:
+//        select
+//        b1_0.bno,
+//                b1_0.content,
+//                b1_0.moddate,
+//                b1_0.regdate,
+//                b1_0.title,
+//                b1_0.writer
+//        from
+//        board b1_0
+//        order by
+//        b1_0.bno desc
+//        limit
+//                ?, ?
+//        Hibernate:
+//        select
+//        count(b1_0.bno)
+//        from
+//        board b1_0
+
+        Page<Board> result = boardRepository.findAll(pageable);
+        // 한 장에 종이에 Board 객체를 가지고 있는 결과는 result에 넣는다.
+        // page class는 다음 페이지 존재 여부, 이전 페이지 존재 여부, 전체 데이터 개수 등등등....계산한다.
+
+        log.info("전체 게시물 수: " +result.getTotalElements()); //99
+        log.info("전체 페이지 수: " + result.getTotalPages()); //10
+        log.info("현제 페이지 번호: " +result.getNumber());
+        log.info("페이지당 데이터 개수: " +result.getSize());
+        log.info("다음 페이지 여부: " + result.hasNext());
+        log.info("시작 페이지 여부: " +result.isFirst());
+
+        // 콘솔에 결과를 출력해보자.
+
+        List<Board> boardList = result.getContent(); // 페이징 처리된 내용을 가져와라
+        boardList.forEach(board -> log.info(board)); //람다식
+        // forEach문은 인덱스를 사용하지 않고 앞에서부터 객체를 리턴함
+        // board -> log.info(board) 람다식: 1개의 명령어가 있을 때 활용
+    }
+}
